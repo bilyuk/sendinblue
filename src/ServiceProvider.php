35436d798119a94c75f2d97c5d00bd80ec644338
bilyuk <bilyukd@gmail.com>
2018-10-11 12:09:14 +0300

    Add service provider

---
+++ src/ServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Juanparati\Sendinblue;
+
+use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
+
+
+class ServiceProvider extends LaravelServiceProvider
+{
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
