b95c09e78383ec7109512f3d6daff8f4f54de06c
bilyuk <bilyukd@gmail.com>
2018-10-11 16:39:38 +0300

    Add client methods

--- src/Client.php
+++ src/Client.php
@@ -2,17 +2,22 @@
 
 namespace bilyuk\Sendinblue;
 
-use SendinBlue\Client\Api\ContactsApi;
 use GuzzleHttp\Client as HTTPClient;
+use SendinBlue\Client\ApiException;
+use SendinBlue\Client\Configuration;
+use SendinBlue\Client\Model\CreateContact;
+use SendinBlue\Client\Model\UpdateContact;
 
 class Client
 {
-    private $apiKey;
     private $client;
+    private $config;
 
-    public function __construct($apiKey)
+    public function __construct($config)
     {
-        $this->apiKey = $apiKey;
+        $this->config = new Configuration();
+        $this->config->setApiKey('api-key', $config['key']);
+
         $this->client = new HTTPClient();
     }
 
@@ -25,14 +30,61 @@ class Client
     {
         $api = 'SendinBlue\\Client\\Api\\' . $api;
 
-        return new $api($this->client);
+        return new $api($this->client, $this->config);
     }
 
-    public function createContact($email, $attributes = [])
+    public function callApi($api, $method, $arguments)
     {
-        return $this->getApi('ContactsApi')->createContact(new \SendinBlue\Client\Model\CreateContact([
+        try {
+            return call_user_func([$this->getApi($api), $method], $arguments);
+        } catch (ApiException $exception) {
+            return false;
+        }
+    }
+
+    /**
+     * @param $email
+     * @param array $attributes
+     * @return bool|mixed
+     */
+    public function create($email, $attributes = [])
+    {
+        return $this->callApi('ContactsApi', 'createContact', new CreateContact([
             'email' => $email,
             'attributes' => $attributes,
         ]));
+    }
+
+    /**
+     * Check if contact exists
+     *
+     * @param $email
+     * @return bool
+     */
+    public function exists($email)
+    {
+        return $this->getInfo($email) !== false;
+    }
+
+    /**
+     * @param $email
+     * @return bool|mixed
+     */
+    public function getInfo($email)
+    {
+        return $this->callApi('ContactsApi', 'getContactInfo', $email);
+    }
+
+    /**
+     * @param $email
+     * @param array $attributes
+     * @return bool|mixed
+     */
+    public function update($email, $attributes = [])
+    {
+        return $this->callApi('ContactsApi', 'updateContact', [
+            $email,
+            new UpdateContact(['attributes' => $attributes])
+        ]);
     }
 }
